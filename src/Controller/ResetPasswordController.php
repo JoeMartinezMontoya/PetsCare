6bed104fee8a44423f3dbaca0bf209651c96d129
Martinez <joe.montoya@outlook.com>
2021-07-23 16:09:56 +0200

    Quickfix typo

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -160,7 +160,7 @@ class ResetPasswordController extends AbstractController
         $email = (new TemplatedEmail())
             ->from(new Address('reset-password@petscare.fr', 'Reset Password Service'))
             ->to($user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Votre demande de réinitialisation')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
